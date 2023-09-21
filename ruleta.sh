2ee8a9ac58365b54290397e7061127d14ceecc32
vazqui12 <daniel.vazquezdecia98@gmail.com>
2023-09-21 17:16:00 +0200

    Subiendo script casino.sh con medio martingala

---
+++ ruleta.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Paleta de Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+
+# Funcion para cerrar proceso al usar control + c
+function ctrl_c(){
+    echo -e "\n\n${redColour}[!] Saliendo...${endColour}\n"
+    tput cnorm;
+    exit 1
+}
+
+# Ctrl+C
+trap ctrl_c INT
+
+# Help Panel (el parametro -e sirve para capturar caracteres especiales)
+function helpPanel(){
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Uso:${endColour}${purpleColour} $0${endColour}\n"
+    echo -e "\t${purpleColour}m)${endColour}${grayColour} Dinero con el que se desea jugar${endColour}\n"
+    echo -e "\t${purpleColour}t)${endColour}${grayColour} Técnica a utilizar${endColour}${purpleColour}(Martingala/inverseLabrouchere)${endColour}\n"
+    echo -e "\t${purpleColour}h)${endColour}${grayColour} Mostrando el panel de ayuda${endColour}\n"
+}
+
+# Plantilla para declarar funciones
+function martingala(){
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Dinero actual: ${endColour}${yellowColour} $money${endColour}\n"
+	echo -ne "\n${yellowColour}[+]${endColour}${grayColour} ¿Cuanto dinero tienes pensado apostar? -> ${endColour}" && read initial_bet
+	echo -ne "\n${yellowColour}[+]${endColour}${grayColour} ¿A que deseas apostar continuamente (par/impar) -> ${endColour}" && read par_impar
+
+	echo -e "\n${yellowColour}[+]${endColour}${grayColour} Vamos a jugar con una cantidad inicial de ${endColour}${yellowColour} $initial_bet${endColour}${grayColour} a${endColour}${yellowColour} $par_impar${endColour}\n"
+
+    tput civis
+    while true; do
+		random_number="$(($RANDOM % 37))" #usamos $RANDOM para generar un numero aletario y ponemos hasta 37 que es el rango de numero que hay en la ruleta, por tanto, queremos que saque numeros entre el 0 y el 36
+		echo -e "\n${yellowColour}[+]${endColour}${grayColour} Ha salido el numero ${endColour}${purpleColour} $random_number${endColour}\n"
+
+		if [ "$(($random_number % 2))" -eq 0 ]; then
+			if [ "$random_number" -eq 0 ]; then
+				echo -e "\n${yellowColour}[+]${endColour}${grayColour} El numero que ha salido 0, por tanto, perdemos${endColour}\n"
+			else
+				echo -e "\n${yellowColour}[+]${endColour}${grayColour} El numero que ha salido es par${endColour}\n"
+			fi
+		else
+			echo -e "\n${yellowColour}[+]${endColour}${grayColour} El numero que ha salido es impar${endColour}\n"
+		fi
+
+
+		sleep 0.4
+
+    done
+
+    tput cnorm
+}
+
+
+
+# Menú
+# m tiene : porque se espera que se proporcione un argumento siempre que se use -m. Con -h no es necesario
+# especificar argumento. La variable arg contiene el valor de la opcion actual que ha sido analizada por
+# getopts.
+while getopts "m:t:h" arg; do
+    case $arg in
+        m) money="$OPTARG";;
+		t) technique="$OPTARG";;
+        h) helpPanel;; # Para cada sentencia hay que poner ;; para cerrarla
+    esac
+done
+
+
+# Sección para llamar a las distintas funciones acorde a cada opción
+if [ $money ] && [ $technique ]; then 
+    if [ "$technique" == "martingala" ]; then
+		martingala
+    else
+		echo -e "\n${redColour}[!] La técnica introducida no es correcta${endColour}\n"
+		helpPanel
+    fi
+else
+	echo -e "\n${redColour}[!] Los datos introducidos son incorrectos${endColour}\n"
+    helpPanel
+fi
+
