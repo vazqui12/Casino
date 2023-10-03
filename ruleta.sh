cd5919c88728769e5e86d4373dea8041b20852eb
Daniel Vázquez <145435572+vazqui12@users.noreply.github.com>
2023-10-03 09:27:03 +0200

    Terminado el código de Martingala

--- ruleta.sh
+++ ruleta.sh
@@ -37,23 +37,47 @@ function martingala(){
 
 	echo -e "\n${yellowColour}[+]${endColour}${grayColour} Vamos a jugar con una cantidad inicial de ${endColour}${yellowColour} $initial_bet${endColour}${grayColour} a${endColour}${yellowColour} $par_impar${endColour}\n"
 
+    backup_bet=$initial_bet
+    play_counter=1
+    jugadas_malas="[ "
+
     tput civis
     while true; do
-		random_number="$(($RANDOM % 37))" #usamos $RANDOM para generar un numero aletario y ponemos hasta 37 que es el rango de numero que hay en la ruleta, por tanto, queremos que saque numeros entre el 0 y el 36
-		echo -e "\n${yellowColour}[+]${endColour}${grayColour} Ha salido el numero ${endColour}${purpleColour} $random_number${endColour}\n"
+		$money=$(($money-$initial_bet))
+        random_number="$(($RANDOM % 37))" #usamos $RANDOM para generar un numero aletario y ponemos hasta 37 que es el rango de numero que hay en la ruleta, por tanto, queremos que saque numeros entre el 0 y el 36
+		
+#       echo -e "\n${yellowColour}[+]${endColour}${grayColour} Ha salido el numero ${endColour}${purpleColour} $random_number${endColour}\n"
+        if [ ! "$money" -le 0 ]; then
+            if [ "$par_impar" == "par" ]; then
+                if [ "$(($random_number % 2))" -eq 0 ]; then
+                    if [ "$random_number" -eq 0 ]; then
+                        initial_bet=$(($initial_bet*2))
+                        jugadas_malas="$random_number"
+#                       echo -e "\n${yellowColour}[+]${endColour}${grayColour} El numero que ha salido 0, por tanto, perdemos${endColour}\n"
+                    else
+#                       echo -e "\n${yellowColour}[+]${endColour}${grayColour} El numero que ha salido es par${endColour}\n"
+                        reward=$(($initial_bet*2))
+                        money=$(($money+$reward))
+                        initial_bet=$backup_bet
+                        jugadas_malas=""
+                    fi
+                else
+#                       echo -e "\n${yellowColour}[+]${endColour}${grayColour} El numero que ha salido es impar${endColour}\n"
+                        initial_bet=$(($initial_bet*2))
+                        jugadas_malas="$random_number"
+                fi
+            fi
+        else
+            echo -e "\n${redColour}[!] Te has quedado sin pasta cabron${endColour}\n"
+            echo -e "\n${yellowColour}[+]${endColour}${grayColour} Ha habido un total de ${endColour}${yellowColour}$play_counter${endColour}${grayColour} jugadas${endColour}\n"
+            echo -e "\n${yellowColour}[+]${endColour}${grayColour} A continuación se van a representar las malas jugadas consecuticas que han salido:${endColour}\n"
+            echo -e "\n${yellowColour}$jugadas_malas${endColour}\n"
+            
+            tput cnorm; 
+            exit 0
+        fi
 
-		if [ "$(($random_number % 2))" -eq 0 ]; then
-			if [ "$random_number" -eq 0 ]; then
-				echo -e "\n${yellowColour}[+]${endColour}${grayColour} El numero que ha salido 0, por tanto, perdemos${endColour}\n"
-			else
-				echo -e "\n${yellowColour}[+]${endColour}${grayColour} El numero que ha salido es par${endColour}\n"
-			fi
-		else
-			echo -e "\n${yellowColour}[+]${endColour}${grayColour} El numero que ha salido es impar${endColour}\n"
-		fi
-
-
-		sleep 0.4
+        let play_counter+=1
 
     done
 
